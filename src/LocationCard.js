d29e5935f2a76e6f4f34264793156f191c693118
Alex  Rutan <alexrutan04@gmail.com>
2022-03-12 19:20:56 -0800

    updated locationCard
    updated to use fontAwesome icons, changes weather state to accept upgraded weather forecast object, and new css styling

--- src/LocationCard.js
+++ src/LocationCard.js
@@ -4,11 +4,16 @@ import FishFileApi from "./Api";
 import UsgsApi from "./UsgsApi";
 import "./LocationCard.css";
 import WeatherCodes from "./WeatherCodeObject";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWind, faUmbrella, faGaugeHigh, faWater } from '@fortawesome/free-solid-svg-icons'
 
 
 function LocationCard({ location }) {
   const [ currFlow, setCurrFlow ] = useState();
-  const [ currWeather, setCurrWeather ] = useState({});
+  const [weather, setWeather] = useState({
+    current: {},
+    forecast: {}
+  });
 
   useEffect(
     function getCurrFlow() {
@@ -24,91 +29,60 @@ function LocationCard({ location }) {
     [location.usgsId]
   )
 
-  // useEffect(
-  //   function getWeather() {
-  //     async function getWeatherResponse() {
-  //       const decLat = String(location.decLat);
-  //       const decLong = String(location.decLong);
-  //       const coords = { decLat, decLong };
-  //       const weather = await FishFileApi.getWeather(location.id, location.username, coords);
-  //       console.log("THIS IS THE WEATHER OBJECT IN REACT APP: ", weather);
+  useEffect(
+    function getWeather() {
+      async function getWeatherResponse() {
+        const decLat = String(location.decLat);
+        const decLong = String(location.decLong);
+        const coords = { decLat, decLong };
+        const weatherResponse = await FishFileApi.getWeather(location.id, location.username, coords);
+        console.log("THIS IS THE WEATHER OBJECT IN REACT APP: ", weatherResponse);
 
-  //       setCurrWeather(weather);
-  //     }
-  //     getWeatherResponse();
-  //   },
-  //   [location.id, location.username, location.decLat, location.decLong]
-  // )
+        setWeather(weatherResponse);
+      }
+      getWeatherResponse();
+    },
+    [location.id, location.username, location.decLat, location.decLong]
+  )
   
   return (
-    // <div className="LocationCard col-lg-4">
-    //   <Link to={`/users/${location.username}/locations/${location.id}`} className="nav-link">
-    //     <div className="card">
-    //       <div className="card-body">
-    //         <h5 className="card-title">{location.name}</h5>
-    //         <h6 className="card-subtitle mb-2 text-muted">{location.usgsId}</h6>
-    //         <div className="row">
-    //           <div className="col-12 col-sm-7">
-    //             <ul className="list-group list-group-flush">
-    //               { currFlow ?
-    //               <li className="list-group-item">Flow: {currFlow} CFS</li>
-    //               :
-    //               null
-    //               }
-    //               <li className="list-group-item">Atmospheric Pressure: {currWeather.pressure} inches Hg</li>
-    //               <li className="list-group-item">Wind: {currWeather.windSpeed} mph</li>
-    //             </ul>
-    //           </div>
-    //           <div className="col-12 col-sm-5">
-    //             <img className="weather-icon-img" src={`/tomorrowApiWeatherIcons/${currWeather.currWeatherCode}.png`} alt="Current Weather Icon"></img>
-    //             <p>{WeatherCodes.currWeather.currWeatherCode</p>
-    //             <div className="row">
-    //               <div className="col-12 col-sm-6">
-    //                 <p>{currWeather.currTemp}°</p>
-    //               </div>
-    //               <div className="col-12 col-sm-6">
-    //                 <p>L: {currWeather.minTemp}°</p>
-    //                 <p>H: {currWeather.maxTemp}°</p>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </Link>
-    // </div>
-
-
-    <div className="LocationCard col-lg-4">
-      <Link to={`/locations/${location.id}`} className="nav-link">
+    <div className="LocationCard col-xl-4 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+      <Link 
+      to={`/locations/${location.id}`}
+      state={{
+        currLocation: location,
+        weather,
+        currFlow
+      }} 
+      className="nav-link">
         <div className="card location-card">
-          <div className="titles-container card-header">
+          <div className="location-titles-container">
             <h5 className="card-title pt-2">{location.name}</h5>
             <p className="card-subtitle text-muted"><small>USGS ID: {location.usgsId}</small></p>
           </div>
           <div className="card-body">
             <div className="row">
-              <div className="col-7 mt-0 weather-left">
+              <div className="col-6 mt-0 weather-left">
                 <ul className="location-card-list-group list-group  list-group-flush">
                   {currFlow ?
                     <li className="list-group-item">
-                      Flow: 
+                      <FontAwesomeIcon icon={faWater} /> 
                       <span className="badge rounded-pill bg-light text-dark">{currFlow} CFS</span>
                     </li>
                     :
                     null
                   }
                   <li className="list-group-item">
-                    Pressure:
-                    <span className="badge rounded-pill bg-light text-dark">28.68 in Hg</span>
+                    <FontAwesomeIcon icon={faGaugeHigh} />
+                    <span className="badge rounded-pill bg-light text-dark">{weather.current.pressure} in Hg</span>
                   </li>
                   <li className="list-group-item">
-                    Wind:
-                    <span className="badge rounded-pill bg-light text-dark">8 mph</span>
+                    <FontAwesomeIcon icon={faWind} />
+                    <span className="badge rounded-pill bg-light text-dark">{weather.current.windSpeed} mph</span>
                   </li>
                   <li className="list-group-item">
-                    Precip. Chance:
-                    <span className="badge rounded-pill bg-light text-dark">30%</span>
+                    <FontAwesomeIcon icon={faUmbrella} />
+                    <span className="badge rounded-pill bg-light text-dark">{weather.current.precipChance}%</span>
                   </li>
                   <li className="list-group-item">
                     Records:
@@ -116,18 +90,18 @@ function LocationCard({ location }) {
                   </li>
                 </ul>
               </div>
-              <div className="col-5 mt-3 text-center weather-right">
+              <div className="col-6 mt-3 text-center weather-right">
                 <div>
-                  <img className="weather-icon-img" src={`/tomorrowApiWeatherIcons/10010.png`} alt="Current Weather Icon"></img>
-                  <p className="mt-2 mb-3"><small>{WeatherCodes["10010"]}</small></p>
+                  <img className="weather-icon-img" src={`/tomorrowApiWeatherIcons/${weather.current.currWeatherCode}.png`} alt="Current Weather Icon"></img>
+                  <p className="mt-2 mb-3"><small>{WeatherCodes[weather.current.currWeatherCode]}</small></p>
                 </div>
-                <div className="row">
-                  <div className="col-6 align-middle">
-                    <h2>60°</h2>
+                <div className="row temps-container">
+                  <div className="col-7 curr-temp-container">
+                    <h1>{weather.current.currTemp}°</h1>
                   </div>
-                  <div className="col-6">
-                    <h6>L: 49°</h6>
-                    <h6>H: 60°</h6>
+                  <div className="high-low-temps-container col-5">
+                    <p className="high-temp"><small>H {weather.current.highTemp}°</small></p>
+                    <p className="low-temp"><small>L {weather.current.lowTemp}°</small></p>
                   </div>
                 </div>
               </div>
